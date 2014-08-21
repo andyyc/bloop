fec9861c06fe67d0993ba37a8568b11ca04e9bb7
EC2 Default User <ec2-user@ip-10-170-26-86.us-west-1.compute.internal>
2014-08-21 09:12:35 +0000

    configuring server and adding some gunicorn scripts

--- fdl/settings.py
+++ fdl/settings.py
@@ -12,10 +12,10 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'dev_fdl',                      # Or path to database file if using sqlite3.
+        'NAME': 'prod_bloop',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'vagrant',
-        'PASSWORD': 'asdfawef',
+        'USER': 'bloopdbuser',
+        'PASSWORD': 'bloopDTBS11',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
@@ -61,7 +61,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/home/ec2-user/www/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -72,7 +72,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/vagrant/fdl/static',
+    # '/vagrant/fdl/static',
+    '/home/ec2-user/www/bloop/static',
 )
 
 # List of finder classes that know how to find static files in
